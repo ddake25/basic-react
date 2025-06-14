4b2dfebe6266539719f3f7a200658c4609927177
Dake Daniel <67493469+coda10@users.noreply.github.com>
2025-06-14 12:25:36 -0400

    Create my first component

---
+++ react-revision/src/App.tsx
@@ -0,0 +1,10 @@
+import Message from "./components/Message";
+
+
+function App() {
+  return (
+    <div> <Message /> </div>
+  );
+}
+
+export default App;
---
+++ react-revision/src/components/Message.tsx
@@ -0,0 +1,11 @@
+
+function Message(){
+    const name = "";
+
+        if(name)
+            return <div><b>HELLO {name}!</b></div>
+        return <div><b>HELLO WORLD!</b></div>
+
+}
+
+export default Message;
